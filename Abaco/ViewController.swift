6069f1deedb2f083fd8d16ed2d701e7e0a412f05
Igor Ferreira <igorcferreira@hotmail.com>
2014-08-16 15:59:54 -0300

    Initial Commit

---
+++ Abaco/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Abaco
+//
+//  Created by Igor Ferreira on 8/16/14.
+//  Copyright (c) 2014 Igor Ferreira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
